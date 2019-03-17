037bb203d7ad575f2646c7743b61d23a593deda9
Fellipe Malta <fellipedm@Fellipes-MacBook-Pro.local>
2019-03-17 13:16:09 -0300

    Fixed MyApp import error on test file

--- flutter_edition_code/test/widget_test.dart
+++ flutter_edition_code/test/widget_test.dart
@@ -6,9 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_edition/src/FlutterEdition.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_edition/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
