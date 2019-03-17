5528aab776c053009aa33654812a1555bce74436
Flavio Farias <flavio@engapp.com.br>
2019-03-17 02:59:39 -0300

    add flutter edition first code version

---
+++ flutter-edition-first-code-version/lib/src/FlutterEdition.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_edition/src/Screens/HomeScreen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
+    );
+  }
+}
